f038688ecc6ec43e4dcad6ca89be95bc448b95bc
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-11-22 02:50:48 +0100

    Use Reassigner class in broker commands

--- lib/ktl/broker.rb
+++ lib/ktl/broker.rb
@@ -10,7 +10,8 @@ module Ktl
         old_leader, new_leader = options.values_at(:from, :to)
         migration_plan = MigrationPlan.new(zk_client, old_leader, new_leader).generate
         say 'moving %d partitions from %d to %d' % [migration_plan.size, old_leader, new_leader]
-        Kafka::Admin.reassign_partitions(zk_client.raw_client, migration_plan)
+        reassigner = Reassigner.new(:migrate, zk_client)
+        reassigner.execute(migration_plan)
       end
     end
 
@@ -34,7 +35,8 @@ module Ktl
       with_zk_client do |zk_client|
         plan = BalancePlan.new(zk_client, regexp).generate
         say 'reassigning %d partitions' % plan.size
-        Kafka::Admin.reassign_partitions(zk_client.raw_client, plan)
+        reassigner = Reassigner.new(:balance, zk_client)
+        reassigner.execute(plan)
       end
     end
 
@@ -43,7 +45,8 @@ module Ktl
       with_zk_client do |zk_client|
         plan = DecommissionPlan.new(zk_client, broker_id.to_i).generate
         say 'reassigning %d partitions' % plan.size
-        Kafka::Admin.reassign_partitions(zk_client.raw_client, plan)
+        reassigner = Reassigner.new(:decommission, zk_client)
+        reassigner.execute(plan)
       end
     end
   end
