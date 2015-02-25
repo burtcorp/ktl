bb58199812292533e3f93685870badba6a214914
Gustav Munkby <grddev@gmail.com>
2015-02-25 12:07:13 +0100

    Cleanup shuffle plan spec
     - Use plain ruby variables brokers, replica_count and assignments as
       base to make it easier to extend the tests
     - Use different id:s for brokers and partitions (to facilitate debug)
     - Generalize the zk client stubs to handle arbitrary input
     - Assert on the structure of the output

--- spec/ktl/shuffle_plan_spec.rb
+++ spec/ktl/shuffle_plan_spec.rb
@@ -18,51 +18,70 @@ module Ktl
     end
 
     describe '#generate' do
-      let :topics do
-        scala_list(%w[topic1 topic2])
-      end
-
       let :brokers do
-        scala_int_list([0, 1])
+        [0xb1, 0xb2]
       end
 
-      let :replica_assignments do
-        ra = Scala::Collection::Mutable::HashMap.new
-        ra.put(Kafka::TopicAndPartition.new('topic1', to_int(0)), scala_int_list([0, 1]))
-        ra.put(Kafka::TopicAndPartition.new('topic1', to_int(1)), scala_int_list([0, 1]))
-        ra.put(Kafka::TopicAndPartition.new('topic2', to_int(0)), scala_int_list([1, 0]))
-        ra.put(Kafka::TopicAndPartition.new('topic2', to_int(1)), scala_int_list([1, 0]))
-        ra
+      let :replica_count do
+        2
       end
 
-      let :topics_partitions do
-        tps = Scala::Collection::Mutable::HashMap.new
-        tps.put('topic1', scala_int_list([0, 1]))
-        tps.put('topic2', scala_int_list([0, 1]))
-        tps
+      let :assignments do
+        {
+          'topic1' => [brokers[0, replica_count], brokers[0, replica_count]],
+          'topic2' => [brokers[0, replica_count].reverse, brokers[0, replica_count].reverse],
+        }
       end
 
       before do
-        allow(zk_client).to receive(:all_topics).and_return(topics)
-        allow(zk_client).to receive(:partitions_for_topics).with(topics).and_return(topics_partitions)
-        allow(zk_client).to receive(:replica_assignment_for_topics).with(topics).and_return(replica_assignments)
-        allow(zk_client).to receive(:broker_ids).and_return(brokers)
+        allow(zk_client).to receive(:all_topics).and_return(scala_list(assignments.keys))
+        allow(zk_client).to receive(:partitions_for_topics) do |scala_topics|
+          ScalaEnumerable.new(scala_topics).each_with_object(Scala::Collection::Mutable::HashMap.new) do |topic, scala_topic_partitions|
+            scala_topic_partitions.put(topic, scala_int_list(assignments[topic].size.times.to_a))
+          end
+        end
+        allow(zk_client).to receive(:replica_assignment_for_topics) do |scala_topics|
+          ScalaEnumerable.new(scala_topics).each_with_object(Scala::Collection::Mutable::HashMap.new) do |topic, scala_assignments|
+            assignments.each do |topic, partition_brokers|
+              partition_brokers.each_with_index do |brokers, partition|
+                scala_assignments.put(Kafka::TopicAndPartition.new(topic, to_int(partition)), scala_int_list(brokers))
+              end
+            end
+          end
+        end
+        allow(zk_client).to receive(:broker_ids).and_return(scala_int_list(brokers))
       end
 
       it 'fetches partitions for topics' do
         plan.generate
-        expect(zk_client).to have_received(:partitions_for_topics).with(topics)
+        expect(zk_client).to have_received(:partitions_for_topics).with(scala_list(assignments.keys))
       end
 
       it 'fetches replica assignments for topics' do
         plan.generate
-        expect(zk_client).to have_received(:replica_assignment_for_topics).with(topics)
+        expect(zk_client).to have_received(:replica_assignment_for_topics).with(scala_list(assignments.keys))
       end
 
       it 'returns a Scala Map with assignments' do
-        generated_plan = plan.generate
-        expect(generated_plan).to be_a(Scala::Collection::Immutable::Map)
-        expect(generated_plan.size).to eq(2)
+        expect(plan.generate).to be_a(Scala::Collection::Immutable::Map)
+      end
+
+      it 'returns a assignments keyed by topic paritions' do
+        plan.generate.keys.foreach do |key|
+          expect(key).to be_a(Kafka::TopicAndPartition)
+        end
+      end
+
+      it 'returns a assignments with broker values' do
+        plan.generate.values.foreach do |value|
+          expect(brokers).to include(*ScalaEnumerable.new(value).to_a)
+        end
+      end
+
+      it 'does not return topic partitions not needing reassignment' do
+        plan.generate.foreach do |tuple|
+          expect(ScalaEnumerable.new(tuple.last).to_a).to_not eq(assignments[tuple.first.topic][tuple.first.partition])
+        end
       end
 
       context 'with a non catch-all filter' do
@@ -70,26 +89,13 @@ module Ktl
           /^topic1$/
         end
 
-        let :filtered_topics do
-          scala_list(%w[topic1])
+        it 'fetches replica assignments only for filtered topics' do
+          plan.generate
+          expect(zk_client).to have_received(:replica_assignment_for_topics).with(scala_list(assignments.keys.grep(filter)))
         end
 
-        let :topics_partitions do
-          tps = Scala::Collection::Mutable::HashMap.new
-          tps.put('topic1', scala_int_list([0, 1]))
-          tps
-        end
-
-        before do
-          allow(zk_client).to receive(:partitions_for_topics).with(filtered_topics).and_return(topics_partitions)
-          allow(zk_client).to receive(:replica_assignment_for_topics).with(filtered_topics).and_return(replica_assignments)
-        end
-
-        it 'only includes filtered topics' do
-          generated_plan = plan.generate
-          expect(generated_plan.size).to be >= 1
-          generated_plan.foreach do |element|
-            expect(element.first.topic).to eq('topic1')
+        it 'does not include topics not included by the filter' do
+          plan.generate.foreach do |element|
             expect(element.first.topic).to_not eq('topic2')
           end
         end
