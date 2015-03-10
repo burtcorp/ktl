905bd555bd182e5cbab5da50388da6310be99141
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-03-10 09:24:13 +0100

    Update arguments to ConsumerOffsetChecker

--- lib/ktl/consumer.rb
+++ lib/ktl/consumer.rb
@@ -6,7 +6,7 @@ module Ktl
     option :topics, type: :array, aliases: %w[-t], default: [], desc: 'list of topics to include (or all if none given)'
     option :zookeeper, aliases: %w[-z], required: true, desc: 'zookeeper uri'
     def lag(group_name)
-      args = %W[--zkconnect #{options.zookeeper} --group #{group_name}]
+      args = %W[--zookeeper #{options.zookeeper} --group #{group_name}]
       args << '--topic' << options.topics.join(',') if options.topics.any?
       Kafka::Tools::ConsumerOffsetChecker.main(args.to_java(:String))
     end
