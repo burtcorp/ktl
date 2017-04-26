f608f4857a640c0cb5d1a7ab0fc536fd853e2851
David Dahl <david@burtcorp.com>
2017-04-26 10:58:15 +0200

    Clean up reassigner logging

--- lib/ktl/continous_reassigner.rb
+++ lib/ktl/continous_reassigner.rb
@@ -13,7 +13,7 @@ module Ktl
     end
 
     def execute(reassignment)
-      Signal.trap('SIGINT', proc { puts 'Exiting due to Ctrl-C'; @latch.count_down })
+      Signal.trap('SIGINT', proc { @logger.info 'Exiting due to Ctrl-C'; @latch.count_down })
       @zk_client.watch_data(zk_utils.class.reassign_partitions_path, self)
       if reassignment_in_progress?
         @logger.info 'reassignment already in progress, watching for changes...'
@@ -29,12 +29,12 @@ module Ktl
       parsed_data = JSON.parse(data)
       if (partitions = parsed_data['partitions'])
         partitions = partitions.map { |r| r.values_at('topic', 'partition').join(':') }
-        puts sprintf('%d partitions left to reassign (%p)', partitions.size, partitions)
+        @logger.info sprintf('%d partitions left to reassign (%p)', partitions.size, partitions)
       else
-        puts sprintf('Data without `partitions` key: %p', parsed_data)
+        @logger.info sprintf('Data without `partitions` key: %p', parsed_data)
       end
     rescue => e
-      puts sprintf('Bad data: %p', data)
+      @logger.error sprintf('Bad data: %p', data)
     end
 
     def handle_data_deleted(path)
@@ -44,7 +44,7 @@ module Ktl
         delete_previous_state
         @latch.count_down
       else
-        puts sprintf('Waiting %ds before next assignment', @delay)
+        @logger.info sprintf('Waiting %ds before next assignment', @delay)
         @sleeper.sleep(@delay * 1000)
         reassign(reassignment)
       end
