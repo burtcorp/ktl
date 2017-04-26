3f5cf289770f67537aa1e2be57aa1d938c666841
David Dahl <david@burtcorp.com>
2017-04-26 09:59:00 +0200

    Add verbose and dryrun to migrate-broker

--- lib/ktl/migration_plan.rb
+++ lib/ktl/migration_plan.rb
@@ -2,10 +2,13 @@
 
 module Ktl
   class MigrationPlan
-    def initialize(zk_client, old_leader, new_leader)
+    def initialize(zk_client, old_leader, new_leader, options = {})
       @zk_client = zk_client
       @old_leader = old_leader.to_java
       @new_leader = new_leader.to_java
+      @options = options
+      @logger = options[:logger] || NullLogger.new
+      @log_plan = !!options[:log_plan]
     end
 
     def generate
@@ -18,6 +21,7 @@ module Ktl
         if replicas.contains?(@old_leader)
           index = replicas.index_of(@old_leader)
           new_replicas = replicas.updated(index, @new_leader, CanBuildFrom)
+          @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
           plan += Scala::Tuple.new(topic_partition, new_replicas)
         end
       end
