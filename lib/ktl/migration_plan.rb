31f263e156bf605f4915b609a78455f4be72b580
David Dahl <david@burtcorp.com>
2017-04-26 17:15:55 +0200

    WIP

--- lib/ktl/migration_plan.rb
+++ lib/ktl/migration_plan.rb
@@ -4,8 +4,8 @@ module Ktl
   class MigrationPlan
     def initialize(zk_client, old_replica, new_replica, options = {})
       @zk_client = zk_client
-      @old_replica = old_replica.to_java
-      @new_replica = new_replica.to_java
+      @old_replica = old_replica
+      @new_replica = new_replica
       @options = options
       @logger = options[:logger] || NullLogger.new
       @log_plan = !!options[:log_plan]
@@ -14,15 +14,23 @@ module Ktl
     def generate
       plan = Scala::Collection::Map.empty
       topics = @zk_client.all_topics
+      p @zk_client.replica_assignment_for_topics(topics)
       assignments = ScalaEnumerable.new(@zk_client.replica_assignment_for_topics(topics))
       assignments.each do |item|
         topic_partition = item.first
-        replicas = item.last
-        if replicas.contains?(@old_replica)
-          index = replicas.index_of(@old_replica)
-          new_replicas = replicas.updated(index, @new_replica, CanBuildFrom)
-          @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
-          plan += Scala::Tuple.new(topic_partition, new_replicas)
+        replicas = Scala::Collection::JavaConversions.as_java_iterable(item.last).to_a
+        # replicas = item.last.to_a
+        p replicas
+        if replicas.contain?(@old_replica)
+          if replicas.contain?(@new_replica)
+            index = replicas.index(@old_replica)
+            # new_replicas = replicas.updated(index, @new_replica, CanBuildFrom)
+            
+            @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
+            plan += Scala::Tuple.new(topic_partition, new_replicas)
+          else
+
+          end
         end
       end
       plan
