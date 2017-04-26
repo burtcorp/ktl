70ec392b61f7f38c6f574245bcb02f9e9b660155
David Dahl <david@burtcorp.com>
2017-04-26 14:49:59 +0200

    Rename leader to replica in MigrationPlan
    It's not actually leaders we're moving

--- lib/ktl/migration_plan.rb
+++ lib/ktl/migration_plan.rb
@@ -2,10 +2,10 @@
 
 module Ktl
   class MigrationPlan
-    def initialize(zk_client, old_leader, new_leader, options = {})
+    def initialize(zk_client, old_replica, new_replica, options = {})
       @zk_client = zk_client
-      @old_leader = old_leader.to_java
-      @new_leader = new_leader.to_java
+      @old_replica = old_replica.to_java
+      @new_replica = new_replica.to_java
       @options = options
       @logger = options[:logger] || NullLogger.new
       @log_plan = !!options[:log_plan]
@@ -18,9 +18,9 @@ module Ktl
       assignments.each do |item|
         topic_partition = item.first
         replicas = item.last
-        if replicas.contains?(@old_leader)
-          index = replicas.index_of(@old_leader)
-          new_replicas = replicas.updated(index, @new_leader, CanBuildFrom)
+        if replicas.contains?(@old_replica)
+          index = replicas.index_of(@old_replica)
+          new_replicas = replicas.updated(index, @new_replica, CanBuildFrom)
           @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
           plan += Scala::Tuple.new(topic_partition, new_replicas)
         end
