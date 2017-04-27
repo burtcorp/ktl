101190c4cd6e029b2a0ff31a7d3d3e1db33d7cf8
David Dahl <david@burtcorp.com>
2017-04-27 14:01:53 +0200

    POC for two step broker migration

--- lib/ktl/migration_plan.rb
+++ lib/ktl/migration_plan.rb
@@ -6,7 +6,6 @@ module Ktl
       @zk_client = zk_client
       @old_replica = old_replica
       @new_replica = new_replica
-      @options = options
       @logger = options[:logger] || NullLogger.new
       @log_plan = !!options[:log_plan]
     end
@@ -14,22 +13,24 @@ module Ktl
     def generate
       plan = Scala::Collection::Map.empty
       topics = @zk_client.all_topics
-      p @zk_client.replica_assignment_for_topics(topics)
       assignments = ScalaEnumerable.new(@zk_client.replica_assignment_for_topics(topics))
       assignments.each do |item|
         topic_partition = item.first
         replicas = Scala::Collection::JavaConversions.as_java_iterable(item.last).to_a
-        # replicas = item.last.to_a
-        p replicas
-        if replicas.contain?(@old_replica)
-          if replicas.contain?(@new_replica)
-            index = replicas.index(@old_replica)
-            # new_replicas = replicas.updated(index, @new_replica, CanBuildFrom)
-            
-            @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
+        if replicas.include?(@old_replica)
+          if replicas.include?(@new_replica)
+            new_replicas = replicas.dup
+            new_replicas.delete(@old_replica)
+            @logger.info "Decreasing #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
+            new_replicas = Scala::Collection::JavaConversions.as_scala_iterable(new_replicas.map {|r| r.to_java}).to_seq
             plan += Scala::Tuple.new(topic_partition, new_replicas)
           else
-
+            new_replicas = replicas.dup
+            old_replica_index = new_replicas.index(@old_replica)
+            new_replicas.insert(old_replica_index, @new_replica)
+            @logger.info "Expanding #{topic_partition.topic},#{topic_partition.partition} from #{replicas} to #{new_replicas}" if @log_plan
+            new_replicas = Scala::Collection::JavaConversions.as_scala_iterable(new_replicas.map {|r| r.to_java}).to_seq
+            plan += Scala::Tuple.new(topic_partition, new_replicas)
           end
         end
       end
