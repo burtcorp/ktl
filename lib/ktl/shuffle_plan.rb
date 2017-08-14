9095aa7e0aa3e08fb8f0f1559df44aedcd9fcdd2
David Dahl <david@burtcorp.com>
2017-08-14 16:33:32 +0200

    Add MinimalMovementShufflePlan, which uses the current state as much as possible, then randomly distributes the rest

--- lib/ktl/shuffle_plan.rb
+++ lib/ktl/shuffle_plan.rb
@@ -16,17 +16,17 @@ module Ktl
       end
       topics_partitions = ScalaEnumerable.new(@zk_client.partitions_for_topics(topics))
       topics_partitions = topics_partitions.sort_by(&:first)
-      replica_assignments = @zk_client.replica_assignment_for_topics(topics)
+      current_replica_assignments = @options[:current_assignment] || @zk_client.replica_assignment_for_topics(topics)
       brokers = select_brokers
       reassignment_plan = Scala::Collection::Map.empty
       topics_partitions.each do |tp|
         topic, partitions = tp.elements
-        nr_replicas = @options[:replication_factor] || replica_assignments.apply(Kafka::TopicAndPartition.new(topic, 0)).size
-        assignment = assign_replicas_to_brokers(topic, brokers, partitions.size, nr_replicas)
+        nr_replicas = @options[:replication_factor] || current_replica_assignments.apply(Kafka::TopicAndPartition.new(topic, 0)).size
+        assignment = assign_replicas_to_brokers(topic, brokers, partitions.size, nr_replicas, current_replica_assignments)
         assignment.each do |pr|
           partition, replicas = pr.elements
           topic_partition = Kafka::TopicAndPartition.new(topic, partition)
-          current_assignment = replica_assignments.apply(topic_partition)
+          current_assignment = current_replica_assignments.apply(topic_partition)
           if current_assignment != replicas || include_all
             @logger.info "Moving #{topic_partition.topic},#{topic_partition.partition} from #{current_assignment} to #{replicas}" if @log_plan
             reassignment_plan += Scala::Tuple.new(topic_partition, replicas)
@@ -54,7 +54,7 @@ module Ktl
       brokers
     end
 
-    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count)
+    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count, _)
       @broker_metadatas ||= begin
         broker_metadatas = Kafka::Admin.get_broker_metadatas(@zk_client, brokers)
         Scala::Collection::JavaConversions.as_scala_iterable(broker_metadatas).to_seq
@@ -66,7 +66,7 @@ module Ktl
   end
 
   class RendezvousShufflePlan < ShufflePlan
-    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count)
+    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count, _)
       if replica_count > brokers.size
         raise ArgumentError, sprintf('replication factor: %i larger than available brokers: %i', replica_count, brokers.size)
       end
@@ -93,7 +93,7 @@ module Ktl
       @rack_mappings = {}
     end
 
-    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count)
+    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count, _)
       if replica_count > brokers.size
         raise ArgumentError, sprintf('replication factor: %i larger than available brokers: %i', replica_count, brokers.size)
       end
@@ -138,7 +138,7 @@ module Ktl
       @leader_count = Hash.new(0)
     end
 
-    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count)
+    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count, _)
       if replica_count > brokers.size
         raise ArgumentError, sprintf('replication factor: %i larger than available brokers: %i', replica_count, brokers.size)
       end
@@ -172,6 +172,67 @@ module Ktl
         @leader_count[selected.first] += 1
         if selected.compact.length != replica_count
           raise ArgumentError, "Unable to find enough available brokers "
+        end
+        result.push(Scala::Tuple.new(partition, Scala::Collection::JavaConversions.as_scala_iterable(selected).to_list))
+      end
+      result
+    end
+
+    private
+
+    def rack_for(broker_id)
+      @rack_mappings[broker_id] ||= Kafka::Admin.get_broker_rack(@zk_client, broker_id)
+    end
+  end
+
+  class MinimalMovementShufflePlan < ShufflePlan
+    def initialize(*args)
+      super
+      @rack_mappings = {}
+      @leader_count = Hash.new(0)
+    end
+
+    def assign_replicas_to_brokers(topic, brokers, partition_count, replica_count, current_replica_assignments_for_topics)
+      if replica_count > brokers.size
+        raise ArgumentError, sprintf('replication factor: %i larger than available brokers: %i', replica_count, brokers.size)
+      end
+      result = []
+      racks = brokers.each_with_object({}) do |broker, acc|
+        rack = rack_for(broker)
+        acc[rack] ||= []
+        acc[rack] << broker
+      end
+      broker_count = Hash.new(0)
+      max_partitions_per_broker = ((partition_count * replica_count) / brokers.size.to_f).ceil
+      max_leader_per_broker = (partition_count / brokers.size.to_f).ceil
+      partition_count.times do |partition|
+        current_assignment = Scala::Collection::JavaConversions.as_java_iterable(current_replica_assignments_for_topics.apply(Kafka::TopicAndPartition.new(topic, partition))).to_a
+        rack_order = racks.keys.dup
+        shift_leader_count = (partition % rack_order.length)
+        move_leaders = rack_order.push(*rack_order.shift(shift_leader_count))
+        rack_order = move_leaders.take(replica_count)
+
+        rack_sorted_brokers = rack_order.map do |rack|
+          rack_brokers = racks[rack].dup
+          current_broker = current_assignment.select {|b| rack_for(b) == rack}.first
+          rack_brokers.delete(current_broker) if current_broker
+          rack_brokers.shuffle!
+          rack_brokers.unshift(current_broker) if current_broker
+          rack_brokers
+        end
+
+        selected = rack_sorted_brokers.map do |rack_brokers|
+          rack_selected_brokers = rack_brokers.select {|broker| broker_count[broker] < max_partitions_per_broker}
+          rack_selected_brokers.first
+        end.sort_by do |broker_id|
+          @leader_count[broker_id]
+        end
+        selected.each do |allocated_broker|
+          broker_count[allocated_broker] += 1
+        end
+        @leader_count[selected.first] += 1
+        if selected.compact.length != replica_count
+          raise ArgumentError, "Unable to find enough available brokers for #{topic} with #{partition_count} partitions and replica factor #{replica_count}. Max partitions per broker: #{max_partitions_per_broker}, current broker count: #{broker_count}, available brokers: #{brokers}"
         end
         result.push(Scala::Tuple.new(partition, Scala::Collection::JavaConversions.as_scala_iterable(selected).to_list))
       end
